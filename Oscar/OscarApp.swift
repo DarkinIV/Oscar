d4187e84bd76297d27141f893411991467a3d2b5
DarkinIV <muhammedasim89@gmail.com>
2025-08-21 15:40:24 +0100

    Initial Commit

---
+++ Oscar/OscarApp.swift
@@ -0,0 +1,17 @@
+//
+//  OscarApp.swift
+//  Oscar
+//
+//  Created by Asim Yilmaz on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct OscarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OscarTests/OscarTests.swift
@@ -0,0 +1,16 @@
+//
+//  OscarTests.swift
+//  OscarTests
+//
+//  Created by Asim Yilmaz on 21/08/2025.
+//
+
+import Testing
+
+struct OscarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
